4223615345417e02221868311fdc0a16d1a10c59
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-06-29 11:16:15 +0100

    added route for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { request } = require('express');
+
+router.get('/me', auth, async (req, res) => {
+try {
+    const profiles = await profiles.findOne({ user: request.user.id}).populate('user', ['name', 'avatar']);
+
+    if(!profile) {
+        return res.status(400).json({msg: 'There is no profile for this user'})
+    }
+    res.json(profile);
+} catch(err) {
+    console.err(err.message);
+    res.status(500).send('Server Error')
+    }
+});
+
 
 module.exports = router;
