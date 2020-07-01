75b44f24d69787af4f150aeec28cd73c96d96812
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-07-01 17:50:13 +0100

    client and server connected

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import NavBar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import './App.css';
+
+const App = () => (
+    <Fragment>
+     <NavBar/> 
+    <Landing />
+    </Fragment>
+);
+
+
+export default App;
