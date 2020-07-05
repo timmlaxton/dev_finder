3851660c6612208a2d51dab5b5135e1f77d2f9e5
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-07-05 07:49:00 +0100

    user authhenticated

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types'
 
 const initialState = {
@@ -15,12 +17,15 @@ export default function( state = initialState, action) {
 
 
     switch(type) {
+        case USER_LOADED:
+           return {...state, authenticated: true, loading: false, user: payload} 
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token)
             return {...state, payload, isAuthenticated: true, loading: false}
-            case REGISTER_FAIL:
+        case REGISTER_FAIL:
+        case AUTH_ERROR:
                 localStorage.removeItem('token');
-                return{...state, token: null, isAuthenticated: false, loading: false}
+            return{...state, token: null, isAuthenticated: false, loading: false}
         default: 
         return state;
     }
