431d1494d646be0beb6efe0cbfad3b8cdb1e3c9c
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-07-09 18:56:29 +0100

    added profile actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,11 @@ import {
     PROFILE_ERROR,
     UPDATE_PROFILE,
     ACCOUNT_DELETED,
-    CLEAR_PROFILE
+    CLEAR_PROFILE,
+    GET_PROFILES,
+    GET_REPOS
+
+
 } from './types';
 
 export const getCurrentProfile = () => async dispatch => {
@@ -15,6 +19,60 @@ export const getCurrentProfile = () => async dispatch => {
       
       dispatch({
           type: GET_PROFILE,
+          payload: res.data
+      });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE});
+
+    try {
+      const res = await axios.get('api/profile');
+      
+      dispatch({
+          type: GET_PROFILES,
+          payload: res.data
+      });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+export const getProfileById = userId => async dispatch => {
+    
+
+    try {
+      const res = await axios.get(`api/profile/${userId}`);
+      
+      dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+      });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+export const getGithubRepos = username => async dispatch => {
+    
+
+    try {
+      const res = await axios.get(`api/profile/github/${username}`);
+      
+      dispatch({
+          type: GET_REPOS,
           payload: res.data
       });
     } catch (err) {
