a39545d1553235781ca9019b2a93cdba217429da
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-07-12 17:06:27 +0100

    added profile top

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { connect} from 'react-redux'
 import Spinner from '../layout/Spinner'
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
 import { getProfileById} from '../../actions/profile'
 
 const Profile = ({ getProfileById, profile: {profile, loading},  auth, match }) => {
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById])
+    }, [getProfileById,match.params.id])
     
     
     return (
@@ -24,6 +26,10 @@ const Profile = ({ getProfileById, profile: {profile, loading},  auth, match }) 
                 Edit Profile
                 </Link>
                 )}
+                 <div class="profile-grid my-1">
+                     <ProfileTop profile={profile}/>
+                     <ProfileAbout profile={profile}/>
+                 </div>
                 </Fragment>}
         </Fragment>
     )
