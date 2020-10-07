f6bf77e68937fcdc8666b67742cbdc2b3a7c641a
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-07 16:30:43 +0100

    cant post profile

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
 const app = express();
 
 connectDB();
 
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false}))
 
-app.get('/', (req, res)  => res.send('API Running'))
+app.get('/', (req, res) => res.send('API Running '))
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, () => console.log(`Server statred on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
